fa3fb58bed4914142e0d67a9e11fe20b4ff67631
Bohdon Sayre <bohdon.sayre@flightschoolstudio.com>
2021-03-26 00:50:21 -0400

    initial commit

---
+++ Source/Picross.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class PicrossTarget : TargetRules
+{
+	public PicrossTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "Picross" } );
+	}
+}
---
+++ Source/Picross/PicrossGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PicrossGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PICROSS_API APicrossGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
